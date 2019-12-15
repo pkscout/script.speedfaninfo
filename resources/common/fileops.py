dd7996d6e8f318a49d141e4f93e466f292cebd6d
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2019-12-15 13:18:03 -1000

    remove unneeded libraries

--- resources/common/fileops.py
+++ resources/common/fileops.py
@@ -1,19 +1,14 @@
-# v.0.6.1
+# v.0.7.0
 
-import os, re, shutil, time
+import os, re
 try:
     _range = range
 except NameError:
     _range = xrange
 try:
-    import subprocess
-    hasSubprocess = True
-except:
-    hasSubprocess = False
-try:
     from kodi_six import xbmcvfs
     isXBMC = True
-except:
+except ImportError:
     isXBMC= False
 
 if isXBMC:
@@ -22,59 +17,62 @@ if isXBMC:
     _exists = xbmcvfs.exists
     _delete = xbmcvfs.delete
     _copy   = xbmcvfs.copy
+    _open   = xbmcvfs.File
 else:
+    import shutil
     _mkdirs = os.makedirs
     _rmdir  = os.rmdir
     _exists = os.path.exists
     _delete = os.remove
     _copy   = shutil.copyfile
+    _open   = open
 
 
-def checkPath( path, create=True ):
+def checkPath( thepath, createdir=True ):
     log_lines = []
-    log_lines.append( 'checking for %s' % path )
-    if not _exists( path ):
-        if create:
-            log_lines.append( '%s does not exist, creating it' % path )
-            _mkdirs( path )
+    log_lines.append( 'checking for %s' % thepath )
+    if not _exists( thepath ):
+        if createdir:
+            log_lines.append( '%s does not exist, creating it' % thepath )
+            _mkdirs( thepath )
         else:
-            log_lines.append( '%s does not exist' % path )
+            log_lines.append( '%s does not exist' % thepath )
         return False, log_lines
     else:
-        log_lines.append( '%s exists' % path )
+        log_lines.append( '%s exists' % thepath )
         return True, log_lines
 
 
-def copyFile( src, dst ):
+def copyFile( thesource, thedest ):
     log_lines = []
-    if _exists( src ):
+    if _exists( thesource ):
         try:
-            log_lines.append( 'copying file %s to %s' % (src, dst) )
-            _copy( src, dst )
+            log_lines.append( 'copying file %s to %s' % (thesource, thedest) )
+            _copy( thesource, thedest )
         except IOError:
-            log_lines.append( 'unable to copy %s to %s' % (src, dst) )
+            log_lines.append( 'unable to copy %s to %s' % (thesource, thedest) )
             return False, log_lines
         except Exception as e:
-            log_lines.append( 'unknown error while attempting to copy %s to %s' % (src, dst) )
+            log_lines.append( 'unknown error while attempting to copy %s to %s' % (thesource, thedest) )
             log_lines.append( e )
             return False, log_lines
         return True, log_lines
     else:
-        log_lines.append( '%s does not exist' % src )
+        log_lines.append( '%s does not exist' % thesource )
         return False, log_lines
 
 
-def deleteFile( src ):
-    return deleteFolder( src, type='file')
+def deleteFile( thesource ):
+    return deleteFolder( thesource, thetype='file')
 
 
-def deleteFolder( src, type='folder' ):
+def deleteFolder( thesource, thetype='folder' ):
     log_lines = []
-    if _exists( src ):
-        if type == 'folder':
+    if _exists( thesource ):
+        if thetype == 'folder':
             #in Mac OSX the .DS_Store file, if present, will block a folder from being deleted, so delete the file
             try:
-                _delete( os.path.join( src, '.DS_Store' ) )
+                _delete( os.path.join( thesource, '.DS_Store' ) )
             except IOError:
                 log_lines.append( 'unable to delete .DS_Store file' )
             except Exception as e:
@@ -84,38 +82,38 @@ def deleteFolder( src, type='folder' ):
         else:
             _action = _delete
         try:
-            log_lines.append( 'deleting %s %s' % (type, src) )
+            log_lines.append( 'deleting %s %s' % (thetype, thesource) )
             if isXBMC:
-                if not _action( src ):
+                if not _action( thesource ):
                     raise IOError( 'unable to delete item' )
             else:
-                _action( src )                
+                _action( thesource )
         except IOError:
-            log_lines.append( 'unable to delete %s' % src )
+            log_lines.append( 'unable to delete %s' % thesource )
             return False, log_lines
         except Exception as e:
-            log_lines.append( 'unknown error while attempting to delete %s' % src )
+            log_lines.append( 'unknown error while attempting to delete %s' % thesource )
             log_lines.append( e )
             return False, log_lines
         return True, log_lines
     else:
-        log_lines.append( '%s does not exist' % src )
+        log_lines.append( '%s does not exist' % thesource )
         return False, log_lines
 
 
-def moveFile( src, dst ):
+def moveFile( thesource, thedest ):
     log_lines = []
     cp_loglines = []
     dl_loglines = []
     success = False
-    if _exists( src ):
-        cp_success, cp_loglines = copyFile( src, dst )
+    if _exists( thesource ):
+        cp_success, cp_loglines = copyFile( thesource, thedest )
         if cp_success:
-            dl_success, dl_loglines = deleteFile( src )
+            dl_success, dl_loglines = deleteFile( thesource )
             if dl_success:
                 success = True
     else:
-        log_lines.append( '%s does not exist' % src)
+        log_lines.append( '%s does not exist' % thesource)
         success = False
     return success, log_lines + cp_loglines + dl_loglines
 
@@ -132,39 +130,10 @@ def naturalKeys( text ):
     return [ atoi( c ) for c in re.split( r'(\d+)', text ) ]
 
 
-def popenWithTimeout( command, timeout ):
-    log_lines = []
-    log_lines.append( 'running command ' + command)
-    if hasSubprocess:
-        try:
-            p = subprocess.Popen( command, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-        except OSError:
-            log_lines.append( 'error finding external script, terminating' )
-            return False, log_lines
-        except Exception as e:
-            log_lines.append( 'unknown error while attempting to run %s' % command )
-            log_lines.append( e )
-            return False, log_lines
-        for t in _range( timeout * 4 ):
-            time.sleep( 0.25 )
-            if p.poll() is not None:
-                return p.communicate(), log_lines
-        p.kill()
-        log_lines.append( 'script took too long to run, terminating' )
-        return False, log_lines
-    else:
-        log_lines.append( 'running command with os.system' )
-        os.system( command )
-        return True, log_lines
-
-
 def readFile( filename ):
     log_lines = []
     if _exists( filename ):
-        try:
-            thefile = xbmcvfs.File( filename, 'r' )
-        except:
-            thefile = open( filename, 'r' )
+        thefile = _open( filename, 'r' )
         try:
             data = thefile.read()
             thefile.close()
@@ -181,18 +150,15 @@ def readFile( filename ):
         return log_lines, ''
 
 
-def renameFile ( src, dst ):
-    return moveFile( src, dst )
+def renameFile ( thesource, thedest ):
+    return moveFile( thesource, thedest )
 
 
 def writeFile( data, filename, wtype='wb' ):
     log_lines = []
     if type(data).__name__=='unicode':
         data = data.encode('utf-8')
-    try:
-        thefile = xbmcvfs.File( filename, wtype )
-    except:
-        thefile = open( filename, wtype )
+        thefile = _open( filename, wtype )
     try:
         thefile.write( data )
         thefile.close()
