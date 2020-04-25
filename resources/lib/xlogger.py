789b5c367a623242089f9a7a203a043f7f2991a7
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-04-25 09:16:59 -1000

    updated common libs

--- resources/lib/xlogger.py
+++ resources/lib/xlogger.py
@@ -1,4 +1,4 @@
-#v.0.4.1
+#v.0.4.3
 
 try:
     from kodi_six import xbmc
@@ -7,10 +7,10 @@ except ImportError:
     import logging, logging.handlers
     LOGTYPE = 'file'
 
-#this class creates an object used to log stuff to the log file
+#this class creates an object used to log stuff to the xbmc log file
 class Logger( object ):
-    def __init__( self, logconfig="file", logformat='%(asctime)-15s %(levelname)-8s %(message)s', logfile='logfile.log',
-                  logname='_logger', numbackups=5, logdebug=False, maxsize=100000, when='midnight', interval=1, preamble='' ):
+    def __init__( self, logconfig="timed", logformat='%(asctime)-15s %(levelname)-8s %(message)s', logfile='logfile.log',
+                  logname='_logger', numbackups=5, logdebug=False, maxsize=100000, when='midnight', preamble='' ):
         self.LOGPREAMBLE = preamble
         self.LOGDEBUG = logdebug
         if LOGTYPE == 'file':
