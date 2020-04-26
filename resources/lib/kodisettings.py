a7fc65ebd062dd80ce43016b7458c280328281fa
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-04-25 17:30:26 -1000

    updated common libraries

--- resources/lib/kodisettings.py
+++ resources/lib/kodisettings.py
@@ -1,43 +1,22 @@
-#v.0.0.1
+#v.0.2.0
 
 def _get_setting( addon, setting_name, default, thetype="string" ):
     if thetype.lower() == "bool":
-        try:
-            thebool = addon.getSettingBool( setting_name )
-            if thebool == 0:
-                return False
-            elif thebool == 1:
-                return True
-            else:
-                return thebool
-        except TypeError:
-            return default
-        except AttributeError:
-            if addon.getSetting( setting_name ).lower() == 'true':
-                return True
-            if addon.getSetting( setting_name ).lower() == 'false':
-                return False
-            return default
+        if addon.getSetting( setting_name ).lower() == 'true':
+            return True
+        if addon.getSetting( setting_name ).lower() == 'false':
+            return False
+        return default
     if thetype.lower() == "int":
         try:
-            return addon.getSettingInt( setting_name )
-        except TypeError:
+            return int( addon.getSetting( setting_name ) )
+        except ValueError:
             return default
-        except AttributeError:
-            try:
-                return int( addon.getSetting( setting_name ) )
-            except ValueError:
-                return default
     if thetype.lower() == "number":
         try:
-            return addon.getSettingNumber( setting_name )
-        except TypeError:
+            return float( addon.getSetting( setting_name ) )
+        except ValueError:
             return default
-        except AttributeError:
-            try:
-                return float( addon.getSetting( setting_name ) )
-            except ValueError:
-                return default
     else:
         setting = addon.getSetting( setting_name )
         if setting:
@@ -51,13 +30,11 @@ def getSettingBool( addon, setting_name, default=False ):
 
 
 def getSettingInt( addon, setting_name, default=0 ):
-    return _get_setting( addon, setting_name, default, 'int')
+    return _get_setting( addon, setting_name, default, 'int' )
 
 
 def getSettingNumber( addon, setting_name, default=0.0 ):
-    return _get_setting( addon, setting_name, default, 'number')
-
+    return _get_setting( addon, setting_name, default, 'number' )
 
 def getSettingString( addon, setting_name, default='' ):
-    return _get_setting( addon, setting_name, default, 'string')
-
+    return _get_setting( addon, setting_name, default, 'string' )
